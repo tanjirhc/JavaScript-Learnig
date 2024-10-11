29f773d537d6bd8e623447c55be7bc01f27fe903
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 22:56:09 +0600

    Arithmatic Operator

--- 06-JS_Operator/app.js
+++ 06-JS_Operator/app.js
@@ -1,67 +1,12 @@
 /* 
-! JavaScript Variable
+! JavaScript Operator
  */
 
-/* 
-* Variable
-*/
-var a = 10;
-var a =20;
-a = 'Bangladesh';
-{
-  var blocked = '30';
-  console.log(a);
-  
-}
-console.log(a);
-console.log(blocked);
-let firstName;
-
-let fruits = 'Orange';
-var country = 'Bangladesh';
-console.log(country);
-
-
-console.log(`${fruits} is my fav, ${fruits} I like to eat`);
 
 /* 
-* let Variable
-! can't be redeclared
-! let is block Scoped
-! let is not hosted
+* Arithmatice Operator
 */
+let a = 10;
+let b = 20;
 
-{
- let b = 10; 
-}
-
-
-b = 11;
-console.log(b);
-// console.log(people);
-// let people = 'someone';
-
-/* const Variable
-! can't be redeclared
-! can't be re-assigned
-! let is block Scoped
-! let is not hosted
-*/
-
-const c = 10;
-console.log(c);
-{
-  const d = 50
-  console.log(c);
-  console.log(d);
-}
-
-const person = {
-  firstName: 'Tanjir', 
-  age: 30,
-};
-
-person.country = 'Bangladesh';
-person.age = 32;
-
-console.log(person);
+console.log(a+b);

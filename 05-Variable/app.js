7063b59865763f7fba0141f55b6822c8263579a6
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 22:24:38 +0600

    clg short command for console.log

--- 05-Variable/app.js
+++ 05-Variable/app.js
@@ -1,6 +1,15 @@
 /* 
 ! JavaScript Variable
  */
+
+/* 
+* Variable
+*/
+var a = 10;
+console.log(a);
+
+let firstName;
+
 let fruits = 'Orange';
 
 console.log(`${fruits} is my fav, ${fruits} I like to eat`);

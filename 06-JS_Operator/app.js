051880f03b230760f5d96e18b083232e29d8729b
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 00:21:55 +0600

    even odd by if else statement

--- 06-JS_Operator/app.js
+++ 06-JS_Operator/app.js
@@ -11,3 +11,12 @@ let b = 20;
 let c = a%b;
 
 console.log(c);
+let userInput = prompt('Enter a Number');
+let number = parseInt(userInput);
+
+if(number % 2 === 0) {
+  console.log(`${number} is even.`);
+}
+else {
+  console.log(`${number} is odd.`);
+}

8c14d763017c0605450b78c8f962a55d37e974a7
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 22:35:54 +0600

    var hosted

--- 05-Variable/app.js
+++ 05-Variable/app.js
@@ -8,10 +8,18 @@
 var a = 10;
 var a =20;
 a = 'Bangladesh';
+{
+  var blocked = '30';
+  console.log(a);
+  
+}
 console.log(a);
-
+console.log(blocked);
 let firstName;
 
 let fruits = 'Orange';
+var country = 'Bangladesh';
+console.log(country);
+
 
 console.log(`${fruits} is my fav, ${fruits} I like to eat`);

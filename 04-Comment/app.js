85c105ab7f9facb6912954f4e052800292feb4dc
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 20:35:14 +0600

    final

---
+++ 04-Comment/app.js
@@ -0,0 +1,13 @@
+let firstName = 'Tanjir';
+let age = 30;
+
+firstName = 'Hasan';
+
+console.log(firstName);
+
+console.log(age + 50);
+
+function funcName() {
+  console.log('My First Function');
+}
+funcName();

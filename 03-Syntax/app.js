dadc0097360aaf4a71c706de1c2dfac4d5a221e4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 18:44:33 +0600

    final

---
+++ 03-Syntax/app.js
@@ -0,0 +1,18 @@
+console.log("hello");
+
+document.getElementById('title').innerHTML = "Hello World";
+
+let sum = 150+40;
+console.log(sum);
+
+if (sum > 100) {
+  console.log('Expensive');
+}
+else {
+  console.log('Cheap');
+}
+
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+sayHello('Tanjir');

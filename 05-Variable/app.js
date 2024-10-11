56195d37a62e87c9921b8998e57eb0ee96666d5a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 21:05:57 +0600

    variable tested in console log

--- 05-Variable/app.js
+++ 05-Variable/app.js
@@ -1,20 +1,6 @@
-// My first Comment -single line comment
-let firstName = 'Tanjir';
-let age = 30;
+/* 
+! JavaScript Variable
+ */
+let fruits = 'mango';
 
-firstName = 'Hasan'; //Changing firstName
-
-console.log(firstName);
-
-console.log(age + 50);
-/* Multi line
-comment
-? Some Info
-! Warning
-TODO Some thing I need to fix
-*/
-
-function funcName() {
-  console.log('My First Function');
-}
-funcName();
+console.log(`${fruits} is my fav, ${fruits} I like to eat`);

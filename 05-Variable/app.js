e21ff221d1cb3e40b0cb0221fb6a7a3b2c661741
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 20:43:50 +0600

    extensions

---
+++ 05-Variable/app.js
@@ -0,0 +1,20 @@
+// My first Comment -single line comment
+let firstName = 'Tanjir';
+let age = 30;
+
+firstName = 'Hasan'; //Changing firstName
+
+console.log(firstName);
+
+console.log(age + 50);
+/* Multi line
+comment
+? Some Info
+! Warning
+TODO Some thing I need to fix
+*/
+
+function funcName() {
+  console.log('My First Function');
+}
+funcName();

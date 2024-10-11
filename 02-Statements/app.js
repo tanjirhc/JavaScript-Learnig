d486be973e34854e56f3f756d1632a653d05e195
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-11 18:32:25 +0600

    JavaScript Document by id

--- 02-Statements/app.js
+++ 02-Statements/app.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+document.getElementById('title').innerHTML = "Hello World";
+
+let sum = 50+40;
+console.log(sum);
+
+if (sum > 100) {
+  console.log('Expensive');
+}
+else {
+  console.log('Cheap');
+}

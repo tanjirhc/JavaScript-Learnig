72c8619e12f9e32e3f28ea89d3ef2ee9bdaf781c
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 11:41:23 +0600

    button click function

--- 09-JS_Operator_Function/app.js
+++ 09-JS_Operator_Function/app.js
@@ -6,3 +6,10 @@ function myFunction(fullName, age, dateOfBirth = 1989) {
 }
 myFunction('Tanjir', 32, 1987);
 myFunction('Hasan', 35, true);
+
+
+function isSubscribed() {
+  console.log('Subscribed');
+}
+
+document.getElementById('btn').addEventListener('click', isSubscribed)

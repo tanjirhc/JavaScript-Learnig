44fc817ba9250457693a92721af6c4c44970eb48
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 16:13:44 +0600

    JS Indexing Array

---
+++ 11-JS_Arrays/app.js
@@ -0,0 +1,10 @@
+/* 
+! JavaScript Arrays
+ */
+const language = ['HTML', 'CSS', 'JavaScript', 'PHP', true, 50];
+
+const language2 = new Array('HTML', 'CSS', 'JavaScript', 'PHP', true, 50);
+
+console.log(language);
+//console.log(language[2]);
+console.log(language2);

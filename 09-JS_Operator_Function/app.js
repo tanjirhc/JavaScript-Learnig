581f5a6a48dce96fffb3b0875758910435563dcb
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 11:53:45 +0600

    function expression

--- 09-JS_Operator_Function/app.js
+++ 09-JS_Operator_Function/app.js
@@ -1,15 +1,31 @@
 /* 
 ! JavaScript Functions
  */
+// --- code driven
 function myFunction(fullName, age, dateOfBirth = 1989) {
   console.log('Hello ' + fullName +' I am '+ age + ' My date of Birth is '+dateOfBirth);
 }
 myFunction('Tanjir', 32, 1987);
 myFunction('Hasan', 35, true);
 
-
+// event driven
 function isSubscribed() {
   console.log('Subscribed');
 }
 
 document.getElementById('btn').addEventListener('click', isSubscribed)
+
+//self driven
+
+// (function () {
+//   console.log('I am Self-Invoking Function');
+// })();
+
+// (function (message) {
+//   console.log('I am self-Invokeing Function', message);
+// })('Hello');
+
+let maths = function(x, y){
+  return x * y;
+};
+console.log(maths(3, 4));

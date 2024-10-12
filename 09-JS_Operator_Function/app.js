2044899d5a8cc88d7052ba08eba129deaa73f260
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 11:34:03 +0600

    arguments

--- 09-JS_Operator_Function/app.js
+++ 09-JS_Operator_Function/app.js
@@ -1,8 +1,8 @@
 /* 
 ! JavaScript Functions
  */
-function myFunction(fullName, age) {
-  console.log('Hello ' + fullName +' I am '+ age);
+function myFunction(fullName, age, dateOfBirth = 1989) {
+  console.log('Hello ' + fullName +' I am '+ age + ' My date of Birth is '+dateOfBirth);
 }
-myFunction('Tanjir', 32);
+myFunction('Tanjir', 32, 1987);
 myFunction('Hasan', 35);

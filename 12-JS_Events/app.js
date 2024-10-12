e57b20049baff8203e2b2c1254a1053392e25424
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 19:13:15 +0600

    fontawesom icons

--- 12-JS_Events/app.js
+++ 12-JS_Events/app.js
@@ -3,4 +3,6 @@
  */
 function subscribe() {
   document.getElementById('text').innerHTML = 'Subscribed';
+  document.getElementById('btn').innerHTML = 'Subscribed';
+  document.getElementById('btn').style.backgroundColor = '#e67e22';
 }

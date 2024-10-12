7d42622616d0413c9370a311aba14699bd1f7cb4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 10:13:57 +0600

    datatypes variable

--- 07-JS_Data_Types/app.js
+++ 07-JS_Data_Types/app.js
@@ -1,5 +1,12 @@
 /* 
 ! JavaScript Datatypes
  */
-let firstName = 'Tanjir';
+let firstName = 'Hasan';
+let fullName = 'My name is "Tanjir"';
 
+let finalName = `My name is ${firstName}`;
+
+console.log(finalName);
+
+console.log(fullName);
+console.log(typeof firstName);

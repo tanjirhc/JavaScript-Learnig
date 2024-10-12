1ae7074d07bc763c298545766629e5da60dfeac0
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 10:20:20 +0600

    JS Calculation

--- 07-JS_Data_Types/app.js
+++ 07-JS_Data_Types/app.js
@@ -6,7 +6,13 @@ let fullName = 'My name is "Tanjir"';
 
 let finalName = `My name is ${firstName}`;
 
+let age = 30;
+let age2 = 35;
+let calculation = age + age2 + firstName + age + age2;
+
+
 console.log(finalName);
 
 console.log(fullName);
-console.log(typeof firstName);
+console.log(typeof calculation);
+console.log(calculation);

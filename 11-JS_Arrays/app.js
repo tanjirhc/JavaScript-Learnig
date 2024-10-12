2a4e066db1061d7103edfc2b3ddf7d5a70d3a3e9
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 16:40:36 +0600

    last item array

--- 11-JS_Arrays/app.js
+++ 11-JS_Arrays/app.js
@@ -10,6 +10,10 @@ const language = ['HTML', 'CSS', 'JavaScript', 'PHP', true, 50];
 
 language[4] = 'Tailwind CSS';
 language[5] = 'React.js';
+language[6] = 'vue.js';
+
+//last item
+console.log(language[language.length-1]);
 
 console.log(language);
 console.log(language.toString());

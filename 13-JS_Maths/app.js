18d5d27852f7d8a93fe471df2809b941d0704849
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 21:07:43 +0600

    bubling

---
+++ 13-JS_Maths/app.js
@@ -0,0 +1,53 @@
+/* 
+! JavaScript Events
+ */
+function subscribe() {
+  document.getElementById('text').innerHTML = 'Subscribed';
+  document.getElementById('btn').innerHTML = 'Subscribed';
+  document.getElementById('btn').style.backgroundColor = '#e67e22';
+}
+
+
+
+function like()  {
+  document.getElementById('like').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked';
+  document.getElementById('like').style.backgroundColor = '#27ae60';
+}
+
+function keyPress() {
+  document.getElementById("text").innerHTML = 'Key Pressed :)';
+}
+
+
+
+// function onLoad() {
+//   alert('Testing JavaScript Onload');
+//   document.getElementById("text").innerHTML = 'Website Loaded Properly';
+// }
+
+function windowResize() {
+  document.getElementById("textarea").style.height = '200px';
+  document.getElementById("form").style.fontFamily = 'Courier New';
+}
+
+function onScroll() {
+  document.getElementById("text").innerHTML = 'Scrolling ';
+}
+
+// document.getElementById('btn').onclick = function () {
+//   document.getElementById('btn').innerHTML = 'Subscribed';
+// }
+
+document.getElementById('ul').addEventListener('click', function (e) {
+  // document.getElementById('ul').innerHTML = 'Subscribed';
+  console.log('UL Clicked');
+},
+true
+);
+
+document.getElementById('li').addEventListener('click', function (e) {
+  // document.getElementById('ul').innerHTML = 'Subscribed';
+  console.log('LI Clicked');
+},
+true
+);

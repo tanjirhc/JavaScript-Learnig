0f75c317e410ebfc67677fa0808eabcae1c59f1e
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 16:17:10 +0600

    array changed

--- 11-JS_Arrays/app.js
+++ 11-JS_Arrays/app.js
@@ -5,6 +5,7 @@ const language = ['HTML', 'CSS', 'JavaScript', 'PHP', true, 50];
 
 //const language2 = new Array('HTML', 'CSS', 'JavaScript', 'PHP', true, 50);
 language[4] = 'Tailwind CSS';
+language[5] = 'React.js';
 
 console.log(language);
 //console.log(language[2]);

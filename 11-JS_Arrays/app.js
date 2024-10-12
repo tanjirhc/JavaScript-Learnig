b451f26cd4e834b761032798ae41673a296a0c2d
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 16:16:28 +0600

    index replacing

--- 11-JS_Arrays/app.js
+++ 11-JS_Arrays/app.js
@@ -3,8 +3,9 @@
  */
 const language = ['HTML', 'CSS', 'JavaScript', 'PHP', true, 50];
 
-const language2 = new Array('HTML', 'CSS', 'JavaScript', 'PHP', true, 50);
+//const language2 = new Array('HTML', 'CSS', 'JavaScript', 'PHP', true, 50);
+language[4] = 'Tailwind CSS';
 
 console.log(language);
 //console.log(language[2]);
-console.log(language2);
+//console.log(language2);

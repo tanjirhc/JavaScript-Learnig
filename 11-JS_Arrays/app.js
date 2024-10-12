5bc949a6b8f90cd280999525e034a42a6fcd0536
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 16:36:09 +0600

    array length

--- 11-JS_Arrays/app.js
+++ 11-JS_Arrays/app.js
@@ -1,13 +1,18 @@
 /* 
 ! JavaScript Arrays
  */
+//const language2 = new Array('HTML', 'CSS', 'JavaScript', 'PHP', true, 50);
+//let firstName = 'Tanjir Hasan';
+//console.log(firstName);
+
 const language = ['HTML', 'CSS', 'JavaScript', 'PHP', true, 50];
 
-//const language2 = new Array('HTML', 'CSS', 'JavaScript', 'PHP', true, 50);
+
 language[4] = 'Tailwind CSS';
 language[5] = 'React.js';
 
 console.log(language);
 console.log(language.toString());
+console.log(language.length);
 //console.log(language[2]);
 //console.log(language2);

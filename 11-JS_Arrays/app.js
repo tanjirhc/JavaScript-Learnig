58f0d0e38bba5bab654a4152fb5f38375e09722f
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 18:17:32 +0600

    array & new Array

--- 11-JS_Arrays/app.js
+++ 11-JS_Arrays/app.js
@@ -11,6 +11,7 @@ const language = ['HTML', 'CSS', 'JavaScript', 'PHP', true, 50];
 language[4] = 'Tailwind CSS';
 language[5] = 'React.js';
 language[6] = 'vue.js';
+language[7] = 'Laravel';
 
 //last item
 console.log(language[language.length-1]);
@@ -20,3 +21,17 @@ console.log(language.toString());
 console.log(language.length);
 //console.log(language[2]);
 //console.log(language2);
+
+
+const person = [];
+person['firstName'] = 'Tanjir';
+person['lastName'] = 'Hasan';
+person[2] = 35;
+console.log(person.length);
+console.log(person);
+console.log(person[2]);
+
+//const num = [20];
+const num = new Array(20, 21);
+
+console.log(num);

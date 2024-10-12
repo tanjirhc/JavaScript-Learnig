75d707c8f2c99e5e1ccbefb4d47445dc5d4c0d36
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-12 19:20:35 +0600

    like function onclick

--- 12-JS_Events/app.js
+++ 12-JS_Events/app.js
@@ -6,3 +6,10 @@ function subscribe() {
   document.getElementById('btn').innerHTML = 'Subscribed';
   document.getElementById('btn').style.backgroundColor = '#e67e22';
 }
+
+
+
+function like()  {
+  document.getElementById('like').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked';
+  document.getElementById('like').style.backgroundColor = '#27ae60';
+}

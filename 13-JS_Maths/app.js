93927e0d6e26ff0ff04098d7a3d7b2f9ce784235
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-13 08:51:38 +0600

    Math.pow

--- 13-JS_Maths/app.js
+++ 13-JS_Maths/app.js
@@ -2,7 +2,11 @@
 ! JavaScript Math
  */
 let num = 30.9564566;
+let num2 = 2;
 
-console.log(Math.round(num));
-console.log(Math.ceil(num));
-console.log(Math.floor(num));
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+
+//square value
+console.log(Math.pow(2,2));

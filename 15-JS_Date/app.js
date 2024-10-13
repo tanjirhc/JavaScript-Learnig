a1902d42512066022f1c8b26ce0425f09c902c59
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-13 09:16:57 +0600

    now()

--- 15-JS_Date/app.js
+++ 15-JS_Date/app.js
@@ -3,7 +3,10 @@
  */
 const myDate = new Date();
 
-console.log(myDate);
-console.log(typeof myDate);
+//console.log(myDate);
+//console.log(typeof myDate);
 //console.log(myDate.toString());
-console.log(myDate.toDateString);
+//console.log(myDate.toDateString);
+
+let myTime = Date.now();
+console.log(myTime);
